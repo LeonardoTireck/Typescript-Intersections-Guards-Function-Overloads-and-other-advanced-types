3fe4a202e1ba1a6b30a1c8531c7a12fe301f6f7d
Leonardo Tireck <leotireck@gmail.com>
2025-04-02 09:39:18 -0300

    add discriminated union

--- guards.ts
+++ guards.ts
@@ -3,16 +3,20 @@
 // Type Guards are especially useful when the function's parameter takes more than one Type or
 // it is assigned the Unknown Type.
 type FileSource = {
+  type: "file";
   path: string;
 };
 const fileSource: FileSource = {
+  type: "file",
   path: "some/path/to/file.txt",
 };
 
 type DBSource = {
+  type: "db";
   connectionUrl: string;
 };
 const dbSource: DBSource = {
+  type: "db",
   connectionUrl: "some-connection-url",
 };
 
@@ -20,12 +24,22 @@ type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
   //now use the Type Guard to determine whether the function parameter type is FileSource or DBSource
-  if ("path" in source) {
-    // this means the source has a path parameter, which tells us the type is FileSource
-    // source.path; -> available
-    return;
-  }
+  //   if ("path" in source) {
+  // this means the source has a path parameter, which tells us the type is FileSource
+  // source.path; -> available
+  //     return;
+  //   }
   // source.connectionUrl; -> this is available here because Typescript understands that because of the
   // if check above, the FileSource case is handled. And because of the return statement, there's no
   // other way for the code to be reached here if the type is not DBSource.
+
+  // Instead of using a property that might change later, you can set a property on the root type that
+  // has a determined value. Like I did on FileSource and DBSource with the property "type". Because
+  // it is a literal type, it's not set to any string, it's set to a specific string.
+  // So now we can Type Guard using that property instead of a property that can change later:
+  if (source.type === "file") {
+    source.path; // -> available
+    return;
+  }
+  source.connectionUrl; // -> available after guard
 }
